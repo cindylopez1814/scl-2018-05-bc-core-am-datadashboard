72e2cc49e227a3eb02e46759ca9c3c6b209383f9
Leslie <leslie.ino@gmail.com>
2018-06-07 10:20:47 -0400

    cambios

---
+++ src/data.js
@@ -0,0 +1,15 @@
+window.computeUsersStats = (user, progress, courses) =>{
+
+};
+
+window.sortUsers = (users, orderBy, orderDirection) =>{
+
+};
+
+window.filterUsers = (users, search) =>{
+
+};
+
+window.processCohortData = (options) =>{
+
+};
