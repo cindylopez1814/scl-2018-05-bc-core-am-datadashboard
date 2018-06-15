8e57a906bdf4f9bf0503aa83cab4ffcc91476679
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-06-15 15:07:39 -0400

    nuevos cambios en js

--- src/data.js
+++ src/data.js
@@ -1,5 +1,19 @@
 window.computeUsersStats = (user, progress, courses) =>{
-
+  fetch(userJSON)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      renderUsers(data);
+    });
+  const renderUsers = data => {
+    btn.addEventListener('click', () => {
+      const render = data.forEach(element => {
+        // element.name === arreglo[i].name
+        return container.innerHTML += `<p>${element.name}</p>`;
+      });
+      return render;
+    });
+  };
 };
 
 window.sortUsers = (users, orderBy, orderDirection) =>{
---
+++ src/main.js
@@ -0,0 +1,2 @@
+const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pwd/users.json';
+const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pwd/progress.json'; 
