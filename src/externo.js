59bee767a91c45899cde4d0840594109447804f6
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-06-15 15:52:02 -0400

    probando enlace

--- src/externo.js
+++ src/externo.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById('boton');
 const tex = document.getElementById('prueba');
-
 btn.addEventListener('click', () => {
   tex.innerHTML = '<h2> Veamos que resulta </h2>';
 });
--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pwd/users.json';
 const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pwd/progress.json'; 
+const btn = document.getElementById('boton');
+const tex = document.getElementById('prueba');
+btn.addEventListener('click', () => {
+  tex.innerHTML = '<h2> Veamos que resulta </h2>';
+});
