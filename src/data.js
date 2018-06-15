671d2bb8da670cfd8ff05c5c210f049ff70b2295
Leslie <leslie.ino@gmail.com>
2018-06-15 14:59:37 -0400

    creacion de id en etiqueta a para darle funcionalidad en js, crecacion de un js externo para guardr los avances sin chocar con el main js

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
-window.computeUsersStats = (user, progress, courses) =>{
+window.computeUsersStats = (user, progress, courses) => {
 
 };
 
-window.sortUsers = (users, orderBy, orderDirection) =>{
+window.sortUsers = (users, orderBy, orderDirection) => {
 
 };
 
-window.filterUsers = (users, search) =>{
+window.filterUsers = (users, search) => {
 
 };
 
-window.processCohortData = (options) =>{
+window.processCohortData = (options) => {
 
 };
