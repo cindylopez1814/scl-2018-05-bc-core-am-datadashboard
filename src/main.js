1f68ad08d6993622ccc92390b65f83799b865832
Leslie <leslie.ino@gmail.com>
2018-06-17 14:30:50 -0400

    arreglo de detalle, pasar los ultimos div a col-9, ya que estaban fuera de este, y la funcion de os botoes cohort no estaban resultan

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pwd/users.json';
 const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pwd/progress.json'; 
+
 const btn = document.getElementById('boton');
 const tex = document.getElementById('prueba');
 btn.addEventListener('click', () => {
