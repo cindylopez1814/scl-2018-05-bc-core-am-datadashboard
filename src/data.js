4138f3c1d751453d04c01a97f3da2e3c97a4de19
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-06-19 12:28:09 -0400

    cambios funcion user

--- src/data.js
+++ src/data.js
@@ -1,20 +1,4 @@
 window.computeUsersStats = (user, progress, courses) =>{
-  fetch(userJSON)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      renderUsers(data);
-    });
-  const renderUsers = data => {
-    btn.addEventListener('click', () => {
-      const render = data.forEach(element => {
-        // element.name === arreglo[i].name
-        return container.innerHTML += `<p>${element.name}</p>`;
-      });
-      return render;
-    });
-  };
-};
 
 window.sortUsers = (users, orderBy, orderDirection) => {
 
