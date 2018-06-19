b5f2781bba48bb5986a2af5a2520330980396731
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-06-19 17:48:52 -0400

    menu estatico, cambion de estilos

--- src/data.js
+++ src/data.js
@@ -1,23 +1,5 @@
 window.computeUsersStats = (user, progress, courses) =>{
-  
-<<<<<<< HEAD
-=======
-  fetch(users)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    renderseUsers(data);
-  });
 
-const renderseUsers = data => {
-  btnTwo.addEventListener('click', () => {
-    const render = data.forEach(element => {
-      return usuarios.innerHTML += `<p>${element.name}</p>`;
-    });
-    return render;
-  });
-};
->>>>>>> 2ab577541398ef7b87f5b303fab2ee778e17b0c3
 };
 window.sortUsers = (users, orderBy, orderDirection) => {
 
@@ -30,3 +12,19 @@ window.filterUsers = (users, search) => {
 window.processCohortData = (options) => {
 
 };
+
+/*fetch(users)
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  renderseUsers(data);
+});
+
+const renderseUsers = data => {
+btnTwo.addEventListener('click', () => {
+  const render = data.forEach(element => {
+    return usuarios.innerHTML += `<p>${element.name}</p>`;
+  });
+  return render;
+});
+};*/
--- src/main.js
+++ src/main.js
@@ -4,12 +4,9 @@ const btnThree = document.getElementById('botonThree');
 const btnFour = document.getElementById('botonFour');
 const tex = document.getElementById('prueba');
 const usuarios = document.getElementById('userReceptor');
-<<<<<<< HEAD
-const user = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-=======
-const users = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
->>>>>>> 2ab577541398ef7b87f5b303fab2ee778e17b0c3
+const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJSON = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; 
+const cohortsJSON = '../data/cohorts/cohorts.json';
 
 
 btn.addEventListener('click', () => {
@@ -17,22 +14,3 @@ btn.addEventListener('click', () => {
 });
 
 
-<<<<<<< HEAD
-fetch(user)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    renderseUsers(data);
-  });
-
-const renderseUsers = data => {
-  btnTwo.addEventListener('click', () => {
-    const render = data.forEach(element => {
-      return usuarios.innerHTML += `<p>${element.name}</p>`;
-    });
-    return render;
-  });
-};
-=======
-
->>>>>>> 2ab577541398ef7b87f5b303fab2ee778e17b0c3
