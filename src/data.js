8984d6ff001be8b58fff181e20439a41fb1866cf
Leslie <leslie.ino@gmail.com>
2018-06-20 09:34:22 -0400

    enlace de users con progress, se muestra en la web ambos juntos, main.js realizado en conjunto con jessica y cindy

--- src/data.js
+++ src/data.js
@@ -1,24 +1,4 @@
-window.onload = () =>{
-  dataJson();
-};
 
-function dataJson() {
-  const users = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-  fetch(users)
-    .then(response => response.json())
-    .then(dataUsers => {
-      console.log(dataUsers);
-      renderseUsers(dataUsers);
-    });
-    
-  const progress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-  fetch(progress)
-    .then(response => response.json())
-    .then(dataProgress => {
-      console.log(dataProgress);
-      renderseProgress(dataProgress);
-    });
-}
 window.computeUsersStats = (user, progress, courses) =>{
   
 };
