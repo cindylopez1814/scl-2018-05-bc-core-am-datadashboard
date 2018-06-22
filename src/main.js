920a17bd75045d0237752e6f3b28d865e6c4a01e
Leslie <leslie.ino@gmail.com>
2018-06-22 01:45:05 -0400

    subiendo readme

--- src/main.js
+++ src/main.js
@@ -1,38 +1,39 @@
-const btn = document.getElementById('boton');
-const btnTwo = document.getElementById('botonTwo');
-const btnThree = document.getElementById('botonThree');
-const btnFour = document.getElementById('botonFour');
-const tex = document.getElementById('prueba');
-const usuarios = document.getElementById('studentsName');
-const urlUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; 
+window.onload = () => {
+  const btn = document.getElementById('boton');
+  const btnTwo = document.getElementById('botonTwo');
+  const btnThree = document.getElementById('botonThree');
+  const btnFour = document.getElementById('botonFour');
+  const tex = document.getElementById('prueba');
+  const usuarios = document.getElementById('studentsName');
+  const urlUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+  const urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json'; 
 
-fetch(urlUsers)
-  .then(response => response.json())
-  .then(users => {
+  fetch(urlUsers)
+    .then(response => response.json())
+    .then(users => {
     // se carga segundo
 
-    fetch(urlProgress)
-      .then(response => response.json())
-      .then(Progress => {
-        console.log(Progress);
-        renderseUsers(users, Progress);
-      });
-  });
+      fetch(urlProgress)
+        .then(response => response.json())
+        .then(Progress => {
+          console.log(Progress);
+          renderseUsers(users, Progress);
+        });
+    });
 
-// se carga primero
-const renderseUsers = (users, Progress) => {
-  let rankingNumber = 0;
-  btn.addEventListener('click', () => {
-    const render = users.forEach(user => {
-      rankingNumber ++;
-      let userProgress = Progress[user.id]; // aqui se hace el match de users.json con progress.json
-      // Cuando se cumpla la condicion entragara el valor correspondiente, si la condicion es falsa, entregara 'sin   info'
-      let percent = 'Sin info';
-      if (userProgress.intro) {
-        percent = userProgress.intro.percent;
-      }
-      return usuarios.innerHTML += '<tr>' +
+  // se carga primero
+  const renderseUsers = (users, Progress) => {
+    let rankingNumber = 0;
+    btn.addEventListener('click', () => {
+      const render = users.forEach(user => {
+        rankingNumber ++;
+        let userProgress = Progress[user.id]; // aqui se hace el match de users.json con progress.json
+        // Cuando se cumpla la condicion entragara el valor correspondiente, si la condicion es falsa, entregara 'sin   info'
+        let percent = 'Sin info';
+        if (userProgress.intro) {
+          percent = userProgress.intro.percent;
+        }
+        return usuarios.innerHTML += '<tr>' +
       '<td>' + rankingNumber + '</td>' +
       '<td>' + user.name.toUpperCase() + '</td>' +
       '<td>' + percent + '</td>' +
@@ -41,7 +42,8 @@ const renderseUsers = (users, Progress) => {
       '<td>' + +'</td>' +
       '<td>' + +'</td>' +
       '</tr>';
+      });
+      return render;
     });
-    return render;
-  });
+  };
 };
