6ee742a4effad11fa51266071456966fb1931225
LeslieInostroza <leslie.ino@gmail.com>
2018-07-23 09:26:35 -0400

    creando rutas de json

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 const btn = document.getElementById('btnLima'); // boton que muestra  a las alumnas
-const usuarios = document.getElementById('studentsName');  // aqui se impirme la tabla
+const usuarios = document.getElementById('studentsName'); // aqui se impirme la tabla
 const contData = document.getElementById('contData');
 let rankingNumber = 0;
+
+
+const userJson = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progresJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const cohortsJson = '../data/cohorts/cohorts.json';
