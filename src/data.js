789becc0f48b32b0d674e7e5f9be3caac59fcc51
LeslieInostroza <leslie.ino@gmail.com>
2018-07-23 20:12:59 -0400

    subiendo lo avanzado durante el dia, hasta ahora, busqueda y codigo principal

--- src/data.js
+++ src/data.js
@@ -1,4 +1,89 @@
 window.computeUsersStats = (users, progress, courses) => {
+  users.forEach((alumnas) => {
+    const alumnasProgress = progress.find(advance => advance[0] === alumnas.id)[1];
+
+    let totalPercent = 0;
+    let lectures = 0;
+    let lecturesCompleted = 0;
+    let percentLectures = 0;
+    let quizzes = 0;
+    let quizzesCompleted = 0;
+    let percentQuizzes = 0;
+    let exercises = 0;
+    let exercisesCompleted = 0;
+    let percentExercises = 0;
+
+    let i;
+    for (i in alumnasProgress) {
+      let element = alumnasProgress[i];
+      let unit;
+
+      for (unit of Object.values(element.units)) {
+        for (let part of Object.values(unit.parts)) {
+          // sacar lectures
+          if (part.type === 'read') {
+            lectures++;
+          }
+          if (part.length === 0) {
+            lectures = 0;
+            totalPercent = 0;
+          }
+          if (part.type === 'read' && part.completed === 1) {
+            lecturesCompleted++;
+          }
+          percentLectures = Math.round((lecturesCompleted * 100) / lectures);
+            
+          // sacar quizzes
+          if (part.type === 'quiz') {
+            quizzes++;
+          }
+          if (part.length === 0) {
+            quizzes = 0;
+            totalPercent = 0;
+          }
+          if (part.type === 'quiz' && part.completed === 1) {
+            quizzesCompleted ++;
+          }
+          percentQuizzes = Math.round((quizzesCompleted * 100) / quizzes);
+
+          // sacar exercises
+          if (part.type === 'practice') {
+            exercises++;
+          }
+          if (part.length === 0) {
+            exercises = 0;
+            totalPercent = 0;
+          }
+          if (part.type === 'practice' && part.completed === 1) {
+            exercisesCompleted++;
+          }
+          percentExercises = Math.round((exercisesCompleted + 100) / exercises);
+          totalPercent = Math.round((percentLectures + percentQuizzes + percentExercises) / 3);
+        }
+      }
+    }
+
+    alumnas.stats = {
+      percentTotal: totalPercent,
+      reads: {
+        total: lectures,
+        completitudLecturas: lecturesCompleted,
+        percentajeLecturas: percentLectures
+      },
+      quizzes: {
+        total: quizzes,
+        completed: quizzesCompleted,
+        percent: percentQuizzes
+      },
+      exercises: {
+        total: exercises,
+        completed: exercisesCompleted,
+        percent: percentLectures
+      },
+    };
+    console.log(alumnas);
+    return users;
+  });
 };
 
 
@@ -7,7 +92,13 @@ window.sortUsers = (users, orderBy, orderDirection) => {
 };
 
 window.filterUsers = (users, search) => {
-
+  if (search) {
+    if (users) {
+      search = search.toLowerCase();
+      return users.filter(user => user && user.name && user.name.toLowerCase().indexOf(search) >= 0);
+    }
+  }
+  return users;
 };
 
 window.processCohortData = (options) => {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-const btn = document.getElementById('btnLima'); // boton que muestra  a las alumnas
-const usuarios = document.getElementById('studentsName'); // aqui se impirme la tabla
+const btnLima = document.getElementById('btnLima'); // boton que muestra  a las alumnas
+const dataAlumnas = document.getElementById('studentsName'); // aqui se impirme la tabla
 const contData = document.getElementById('contData');
 let rankingNumber = 0;
+
+let users = null;
+let progress = null;
+let courses = null;
 
 // rutas para acceder a los json
 const usersJson = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJson = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const cohortsJson = '../data/cohorts/cohorts.json';
+const cohortsJson = '../data/cohorts.json';
 
 Promise.all([
   fetch(usersJson),
@@ -16,11 +20,33 @@ Promise.all([
   (responses) => { // engloba todas las promesas
     return Promise.all(responses.map((response) => {
       return response.json();
-      console.log(response.json);
     }));
   }
 ).then((responseJSON)=>{
   users = responseJSON[0];
   progress = Object.entries(responseJSON[1]);
+  courses = Object.entries(responseJSON[2]);
 });
 
+btnLima.addEventListener('click', () => {
+  computeUsersStats(users, progress, courses);
+  users.forEach((user) => {
+    if (user.stats.exercises.length === 0 && user.stats.quizzes.length === 0 && user.stats.reads.length === 0 && user.stats.percentTotal.length === 0) {
+      user.stats.percentTotal = 0;
+      user.stats.reads = {};
+      user.stats.exercises = {};
+      user.stats.quizzes = {};
+    } else {
+      dataAlumnas.innerHTML += `<p>${user.name.toUpperCase()} ${user.stats.percentTotal}  ${JSON.stringify(user.stats.reads.total)}  ${JSON.stringify(user.stats.exercises.total)}  ${JSON.stringify(user.stats.quizzes.total)}</p>`;
+    }
+  });
+});
+
+function searchStudents() {
+  const search = searchAlumnas.value;
+  const filteredUsers = window.filterUsers(users, search);
+  dataAlumnas.innerHTML = '';
+  filteredUsers.forEach(user => {
+    dataAlumnas.innerHTML += `<p>${user.name}</p>`;
+  });
+}
